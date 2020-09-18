f735756109e176d8bc5a45b0277fb73f6c359fc9
akazlow <akazlow@microsoft.com>
2020-09-18 12:48:27 -0400

    test pipeline run

--- iac/test.tf
+++ iac/test.tf
@@ -7,5 +7,5 @@ terraform {
 
 # website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
 output "hello_world" {
-  value = "Hello, World!!!"
+  value = "Hello, World!!! AGAIN!"
 }
