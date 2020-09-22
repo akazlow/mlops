c28d2307eb5a1602e7631e8cbc4d993b1f62fa9f
akazlow <akazlow@microsoft.com>
2020-09-22 12:13:13 -0400

    Update test.tf

--- iac/sub_folder/test.tf
+++ iac/sub_folder/test.tf
@@ -7,5 +7,5 @@ terraform {
 
 # website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
 output "hello_world" {
-  value = "Hello, World!!! AGAIN!"
+  value = "Hello, World!!! AGAIN!!"
 }
